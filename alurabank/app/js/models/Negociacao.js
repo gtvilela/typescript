0fa73edca8ac334c06f09cde1e7f5cdda3881c47
Gabriela Vilela <37645721+gtvilela@users.noreply.github.com>
2020-02-19 13:15:19 -0300

    Add files via upload
    Atualização do curso typescript

--- alurabank/app/js/models/Negociacao.js
+++ alurabank/app/js/models/Negociacao.js
@@ -4,23 +4,16 @@ class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     }
-
     get data() {
         return this._data;
     }
-
     get quantidade() {
         return this._quantidade;
     }
-
     get valor() {
         return this._valor;
     }
-
-
     get volume() {
         return this._quantidade * this._valor;
     }
-
-
 }
